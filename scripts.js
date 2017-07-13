14203c4cca7b0ca7157d66928906c497047740cc
Kelly Miller <kellyannemiller@gmail.com>
2017-07-13 07:14:35 -0600

    start accordian

---
+++ scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $('#tab-nav').children('.tab-list-item').first().children('.acc-link').addClass('active')
+        .next().addClass('is-open').show();
+
+    $('#tab-nav').on('click', '.acc-link', function() {
+
+      if (!$(this).hasClass('active')) {
+
+        $('#tab-nav .is-open').removeClass('is-open').hide();
+        $(this).next().toggleClass('is-open').toggle();
+
+        $('#tab-nav').find('.active').removeClass('active');
+        $(this).addClass('active');
+      } else {
+        $('#tab-nav .is-open').removeClass('is-open').hide();
+        $(this).removeClass('active');
+      }
+   });
+});
